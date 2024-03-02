07b1fde33e74c1cc638f6554150147b3f80eb0a1
Y-admen <yassminash305@gmail.com>
2024-03-02 18:28:11 +0200

    cls

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,16 +9,14 @@ class Rectangle:
 
         self.__width = width
         self.__height = height
-    
-    
+
     @property
     def width(self):
         """Width retriver.
         Returns:
             int: the width of the rectangle."""
         return self.__width
-    
-    
+
     @width.setter
     def width(self, value):
         """Property setter for height of rectangle.
@@ -36,7 +34,6 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-
     @property
     def height(self):
         """height retriver.
@@ -44,7 +41,6 @@ class Rectangle:
             Returns:
                 int: the height of the rectangle."""
         return self.__height
-
 
     @height.setter
     def height(self, value):
