c850fff44d793cd0efa6caa9d08fbcf2942e8c7a
Y-admen <yassminash305@gmail.com>
2024-03-03 00:32:37 +0200

    almost cir

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+"""the class base file"""
 
 
 class Base:
+    """the base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """initioalization method"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = id
-
-        
-        
