158e65e72255c1f133eb3daf6d50f374e64a7118
Y-admen <yassminash305@gmail.com>
2024-03-04 00:13:05 +0200

    up

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,6 @@
+def add_integer(a, b=98):
+    if a and b isnumeric'''check if a and b is numeric
+    bard:
+    def are_both_numbers(var1, var2):
+    return isinstance(var1, (int, float)) and isinstance(var2, (int, float))'''
+    return(a + b)
