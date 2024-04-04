eb134cd6c4760ea1c03c9ebad71f45aed1a8be01
Y-admen <yassminash305@gmail.com>
2024-04-04 11:36:06 +0200

    check2

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,5 @@
 -- creates a table second_table in the database hbtn_0c_0
-CREATE TABLE if NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
-);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table VALUES (1, "John", 10),
 				(2, "Alex", 3),
 				(3, "Bob", 1),
