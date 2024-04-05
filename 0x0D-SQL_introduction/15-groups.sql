7b45a910caa5463d46ddf007da22a58a126655a9
Y-admen <yassminash305@gmail.com>
2024-04-05 07:56:58 +0200

    mandatory

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,1 +1,4 @@
+-- This query selects the score and count of scores from the second_table table, 
+-- groups the results by score, orders them descending by score, 
+-- and aliases the count of scores column as 'number'.
 SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,1 +1,4 @@
+/*
+Retrieves the names and scores of the rows in second_table where name is not NULL, ordered by score in descending order.
+*/
 SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score  DESC;
