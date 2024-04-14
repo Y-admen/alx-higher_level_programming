a09a362aa7da9d538363985bf1fe62345d815038
Y-admen <yassminash305@gmail.com>
2024-04-14 05:49:49 +0200

    fix1,14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 
+
 class City(Base):
     """City class that inherits from Base"""
     __tablename__ = 'cities'
