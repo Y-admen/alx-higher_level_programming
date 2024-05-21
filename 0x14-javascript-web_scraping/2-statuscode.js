a81473413b07995b51737ecaf902594fa56267cb
Y-admen <yassminash305@gmail.com>
2024-05-22 01:47:42 +0300

    task2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const req = require('request');
+const url = process.argv[2];
+req(url, (error, response) => {
+  if (error) {
+    console.err(error);
+  } else {
+    console.log('code: ', response.statusCode);
+  }
+});
