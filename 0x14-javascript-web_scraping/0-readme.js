4c64df342d595817805907a66a84f31f10373d63
Y-admen <yassminash305@gmail.com>
2024-05-22 01:04:47 +0300

    task0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs'); // Import the File System module
+
+const path = process.argv[2]; // Get the file path from command line arguments
+
+fs.readFile(path, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err); // Print the error object
+  } else {
+    console.log(data); // Print the file content
+  }
+});
