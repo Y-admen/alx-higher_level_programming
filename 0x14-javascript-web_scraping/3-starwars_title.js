d5ad58227b943c90b495f14a59fd75590bc584ce
Y-admen <yassminash305@gmail.com>
2024-05-22 02:10:26 +0300

    task3.1

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,11 +4,11 @@ const req = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 
-req(url ,(error, response, body) =>{
-	if (error) {
-		console.error(error);
-	} else{
-		const data = JSON.parse(body);
-		console.log(data.title);
-	}
+req(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
 });
