c26fed2d4f25f106b0a5c1037455e7d50275df97
Y-admen <yassminash305@gmail.com>
2024-05-22 03:42:10 +0300

    task4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const req = require('request');
+const url = process.argv[2];
+let count = 0;
+req(url, (error, response, body) => {
+  const obj = JSON.parse(body);
+  const results = obj.results; // accessing the result
+  results.forEach(film => { // looping through result
+    if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
+    }
+  });
+  console.log(error || count);
+});
