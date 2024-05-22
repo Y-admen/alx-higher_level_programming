2b8bfd98a0d9b2133e037d2d8b2287a9bfa8179f
Y-admen <yassminash305@gmail.com>
2024-05-22 03:45:23 +0300

    task4.1

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,6 +4,9 @@ const req = require('request');
 const url = process.argv[2];
 let count = 0;
 req(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
   const obj = JSON.parse(body);
   const results = obj.results; // accessing the result
   results.forEach(film => { // looping through result
